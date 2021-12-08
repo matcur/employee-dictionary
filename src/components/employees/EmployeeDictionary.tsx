32c8dd859456cb39d8a8aaf535a2bcdeb294af04
matsur <ru.far2014@gmail.com>
2021-12-08 20:11:30 +0500

    upd: change NewEmployeeForm to BaseEmployee form in EmployeeDictionary

--- src/components/employees/EmployeeDictionary.tsx
+++ src/components/employees/EmployeeDictionary.tsx
@@ -1,26 +1,22 @@
 import {observer} from "mobx-react";
 import {useStores} from "../../stores";
 import {EmployeeList} from "./EmployeeList";
-import {NewEmployeeForm} from "./NewEmployeeForm";
-import {NewEmployee} from "../../models";
-import {maxId} from "../../utils/maxId";
+import {BaseEmployeeForm} from "./BaseEmployeeForm";
 
 export const EmployeeDictionary = observer(() => {
   const stores = useStores();
   const employeeStore = stores.employee;
   const employees = employeeStore.employees;
 
-  const addEmployee = (newEmployee: NewEmployee) => {
-    const employee = {...newEmployee, id: maxId(employees)};
-    employeeStore.add(employee);
-  }
-
   return (
     <div className="employee-dictionary">
-      <NewEmployeeForm
-        addEmployee={addEmployee}
+      <BaseEmployeeForm
+        onSave={employeeStore.add.bind(employeeStore)}
         employees={employees}/>
       <EmployeeList employees={employees}/>
+      <BaseEmployeeForm
+        onSave={employeeStore.update.bind(employeeStore)}
+        employees={employees}/>
     </div>
   )
 })
